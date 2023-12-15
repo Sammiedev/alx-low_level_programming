c96288fd1d157d606e9f989fa32a09f93862f6df
sammiedev <officialsamueludoro@gmail.com>
2023-12-14 18:40:29 -0800

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - A program that print a line using the function printf funtion
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("program that prints exactly with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
